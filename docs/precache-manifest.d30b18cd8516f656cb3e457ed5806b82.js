f47c16db3531f9212856dfce9a9dbaeeda9968b6
harish <khot.harish1207@gmail.com>
2021-02-20 11:42:21 +0530

    minor updates

---
+++ docs/precache-manifest.d30b18cd8516f656cb3e457ed5806b82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b29e331b009d7c84589b92508862c04",
+    "url": "./index.html"
+  },
+  {
+    "revision": "feebdcd6422b8ea203fb",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "babe04d6565241feb750",
+    "url": "./static/css/main.98a5b280.chunk.css"
+  },
+  {
+    "revision": "feebdcd6422b8ea203fb",
+    "url": "./static/js/2.2f41e427.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "./static/js/2.2f41e427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "babe04d6565241feb750",
+    "url": "./static/js/main.9d5377cb.chunk.js"
+  },
+  {
+    "revision": "e957350ced5521dd74d9",
+    "url": "./static/js/runtime-main.85e4f3b6.js"
+  }
+]);
